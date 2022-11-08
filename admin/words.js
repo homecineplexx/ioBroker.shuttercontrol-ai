1a35fed8544772cd9c641d2eceb9e03fc0ad62b6
Christian Koller-Kink <chris.homecineplexx@gmail.com>
2022-11-08 16:10:17 +0100

    Initial

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'shuttercontrol-ai adapter settings': {
+		'en': 'Adapter settings for shuttercontrol-ai',
+		'de': 'Adaptereinstellungen für shuttercontrol-ai',
+		'ru': 'Настройки адаптера для shuttercontrol-ai',
+		'pt': 'Configurações do adaptador para shuttercontrol-ai',
+		'nl': 'Adapterinstellingen voor shuttercontrol-ai',
+		'fr': "Paramètres d'adaptateur pour shuttercontrol-ai",
+		'it': "Impostazioni dell'adattatore per shuttercontrol-ai",
+		'es': 'Ajustes del adaptador para shuttercontrol-ai',
+		'pl': 'Ustawienia adaptera dla shuttercontrol-ai',
+		'zh-cn': 'shuttercontrol-ai的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
